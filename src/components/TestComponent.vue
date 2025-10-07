efe91c7263fcd7aa08d47399f6fab7ff5d9a644f
dominik.halvonik <dominik.halvonik@gmail.com>
2025-10-07 14:11:25 +0200

    Add test component

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "TestComponent",
+  data() {
+    return {
+      cislo: 1,
+      meno: "Dominik",
+      priezvisko: "Martikan"
+    }
+  },
+  methods: {
+    increment() {
+      this.cislo++
+    },
+    decrement() {
+      this.cislo--
+    }
+  },
+  computed: {
+    getColor() {
+      if(this.cislo < 5) {
+        return "green"
+      } else {
+        return "blue"
+      }
+    },
+    getMyName() {
+      return this.meno + " " + this.priezvisko
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <p>Ahoj toto je moj prvy komponent</p>
+    <p>{{ meno }} {{ priezvisko }}</p>
+    <p>{{ cislo }}</p>
+    <p>{{ getMyName }}</p>
+    <p :class="getColor">{{ getMyName }}</p>
+    <button @click="increment">Pridaj</button>
+    <button @click="decrement">Uber</button>
+  </div>
+</template>
+
+<style scoped>
+.green {
+  color: green;
+  font-size: 50px;
+}
+
+.blue {
+  color: blue;
+  font-size: 30px;
+}
+</style>
